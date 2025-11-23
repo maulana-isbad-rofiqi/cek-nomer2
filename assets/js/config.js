d1cf24f9cc399909a59cbec5e9f399e918ae747e
maulana isbad rofiqi <147914104+maulana-isbad-rofiqi@users.noreply.github.com>
2025-11-23 21:56:58 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,5 @@
 const CONFIG = {
-  // Ganti dengan Token Bot Telegram Anda
   TELEGRAM_BOT_TOKEN: "8058460969:AAHJgUmeY8gs27taEVPiiLyGvZoxh20jfec", 
-  
-  // Ganti dengan Chat ID Penerima
   TELEGRAM_CHAT_ID: "5261918243",
-  
-  // ID Google AdSense
   ADSENSE_ID: "ca-pub-6900623206484427"
 };
